1db5293f94b6e0dda298937ade48e2eb5d73116b
Kazik <kazik@example.com>
2025-11-11 10:19:40 +0100

    updated reading functions to make them more reusable

--- utils.py
+++ utils.py
@@ -5,11 +5,17 @@ from constants import PROJECT_ROOT
 EN_DATA_PATH = PROJECT_ROOT / "data"
 
 
-def read_en_humanitarian_data(dataset: str = "all_data_en") -> tuple:
+def read_en_humanitarian_data(
+    dataset: str = "all_data_en",
+    basename: str = "crisis_consolidated_humanitarian_filtered_lang_en",
+    filetype: str = ".tsv",
+) -> tuple:
     """Reads English humanitarian data.
 
     Args:
         dataset (str): Name of the English dataset file. Defaults to "all_data_en".
+        basename (str): Base name of the data files. Defaults to "crisis_consolidated_humanitarian_filtered_lang_en".
+        filetype (str): Type of the data file. Defaults to ".tsv".
 
     Returns:
         (
@@ -18,22 +24,20 @@ def read_en_humanitarian_data(dataset: str = "all_data_en") -> tuple:
             test_df: pd.DataFrame
         )
     """
-    humanitarian_data = "crisis_consolidated_humanitarian_filtered_lang_en"
-
     humanitarian_train_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{humanitarian_data}_train.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_train{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
     )
     humanitarian_dev_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{humanitarian_data}_dev.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_dev{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
     )
     humanitarian_test_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{humanitarian_data}_test.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_test{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
@@ -46,11 +50,17 @@ def read_en_humanitarian_data(dataset: str = "all_data_en") -> tuple:
     )
 
 
-def read_en_informativeness_data(dataset: str = "all_data_en") -> tuple:
+def read_en_informativeness_data(
+    dataset: str = "all_data_en",
+    basename: str = "crisis_consolidated_informativeness_filtered_lang_en",
+    filetype: str = ".tsv",
+) -> tuple:
     """Reads English informativeness data.
 
     Args:
         dataset (str): Name of the English dataset file. Defaults to "all_data_en".
+        basename (str): Base name of the data files. Defaults to "crisis_consolidated_informativeness_filtered_lang_en".
+        filetype (str): Type of the data file. Defaults to ".tsv".
 
     Returns:
         (
@@ -59,22 +69,21 @@ def read_en_informativeness_data(dataset: str = "all_data_en") -> tuple:
             test_df: pd.DataFrame
         )
     """
-    informativeness_data = "crisis_consolidated_informativeness_filtered_lang_en"
 
     informativeness_train_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{informativeness_data}_train.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_train{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
     )
     informativeness_dev_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{informativeness_data}_dev.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_dev{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
     )
     informativeness_test_df = pd.read_csv(
-        EN_DATA_PATH / dataset / f"{informativeness_data}_test.tsv",
+        EN_DATA_PATH / dataset / f"{basename}_test{filetype}",
         sep="\t",
         engine="python",
         on_bad_lines="skip",
