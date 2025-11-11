f996a22bde532d524cbd8cef0364d16a99eb8231
Kazik <kazik@example.com>
2025-11-11 10:57:47 +0100

    moved dataset creating functions to utils

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 from constants import PROJECT_ROOT
+from dataset import TweetDataset
 
 EN_DATA_PATH = PROJECT_ROOT / "data"
+CLEAN_DATA_PATH = EN_DATA_PATH / "clean_en_data"
 
 
 def read_en_humanitarian_data(
@@ -97,3 +99,35 @@ def read_en_informativeness_data(
         informativeness_dev_df,
         informativeness_test_df,
     )
+
+
+def get_human_datasets():
+    """Creates and returns torch.utils.data.Dataset's for the humanitarian data.
+    Returns:
+        (
+            train_df: torch.utils.data.Dataset,
+            dev_df: torch.utils.data.Dataset,
+            test_df: torch.utils.data.Dataset
+        )
+    """
+    human_train_df, human_dev_df, human_test_df = read_en_humanitarian_data(
+        dataset=CLEAN_DATA_PATH, basename="human", filetype=".csv"
+    )
+
+    return (TweetDataset(human_train_df), TweetDataset(human_dev_df), TweetDataset(human_test_df))
+
+
+def get_info_datasets():
+    """Creates and returns torch.utils.data.Dataset's for the informativeness data.
+    Returns:
+        (
+            train_df: torch.utils.data.Dataset,
+            dev_df: torch.utils.data.Dataset,
+            test_df: torch.utils.data.Dataset
+        )
+    """
+    info_train_df, info_dev_df, info_test_df = read_en_informativeness_data(
+        dataset=CLEAN_DATA_PATH, basename="info", filetype=".csv"
+    )
+
+    return (TweetDataset(info_train_df), TweetDataset(info_dev_df), TweetDataset(info_test_df))
