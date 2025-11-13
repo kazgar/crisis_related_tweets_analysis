76ead51d77288f149cfd57a501a6dddfbe68079c
Kazik <kazik@example.com>
2025-11-13 09:56:57 +0100

    created inverse weights func for imbalanced datasets

--- dataset.py
+++ dataset.py
@@ -7,16 +7,28 @@ from transformers import AutoTokenizer
 
 
 class TweetDataset(Dataset):
-    def __init__(self, csv_file: Path | pd.DataFrame, max_len=128):
+    def __init__(self, csv_file: Path | pd.DataFrame, max_len: int = 128):
         if isinstance(csv_file, Path):
             self.tweet_data = pd.read_csv(csv_file)
         elif isinstance(csv_file, pd.DataFrame):
             self.tweet_data = csv_file
 
+        self.weighing = weighing
         self.texts = self.tweet_data["text"].tolist()
         self.labels = self.tweet_data["class_label"].tolist()
         self.tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=True)
         self.max_len = max_len
+
+    def get_inverse_weights(self):
+        label_counts = self.tweet_data["class_label"].value_counts().sort_index()
+
+        inverse_freqs = 1.0 / label_counts
+
+        normalized_weights = inverse_freqs / inverse_freqs.sum()
+
+        weights_tensor = torch.tensor(normalized_weights.values, dtype=torch.float)
+
+        return weights_tensor
 
     def __len__(self):
         return len(self.tweet_data)
