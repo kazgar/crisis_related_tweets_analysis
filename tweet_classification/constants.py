4f7ad7acb72f6f8d2138a7b68f396ec30462c84a
Kazik <kazik@example.com>
2025-11-14 12:21:22 +0100

    moved hyperparameters and paths to constants.py

--- tweet_classification/constants.py
+++ tweet_classification/constants.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import torch
+
 EXPERIMENT_NR = 1
 
 PROJECT_ROOT = Path(__file__).parent.parent.resolve()
@@ -8,3 +10,14 @@ EN_DATA_PATH = DATA_PATH / "all_data_en"
 CLEAN_DATA_PATH = DATA_PATH / "clean_en_data"
 RESULTS_PATH = PROJECT_ROOT / "results"
 GRAPHS_PATH = PROJECT_ROOT / "graphs"
+
+NUM_WORKERS = 1
+DROPOUT = 0.1
+SEED = 42
+NUM_EPOCHS = 50
+BATCH_SIZE = 64
+DEVICE = (
+    torch.device("cuda")
+    if torch.cuda.is_available()
+    else torch.device("mps") if torch.mps.is_available() else torch.device("cpu")
+)
--- tweet_classification/human_train.py
+++ tweet_classification/human_train.py
@@ -1,47 +1,40 @@
 import os
 
+import constants as const
 import pandas as pd
 import torch
+from classifier import TweetClassifier
 from torch import nn
 from torch.utils.data import DataLoader
-
-from classifier import TweetClassifier
-from constants import EXPERIMENT_NR, PROJECT_ROOT
 from train_test_funcs import train
 from utils import get_human_datasets, read_en_humanitarian_data
 
-HUMAN_RESULT_PATH = PROJECT_ROOT / "results" / "human_results"
-DEVICE = "cuda" if torch.cuda.is_available() else "mps" if torch.mps.is_available() else "cpu"
-BATCH_SIZE = 64
-NUM_WORKERS = 1
-RANDOM_SEED = 42
-DROPOUT = 0.1
-NUM_EPOCHS = 50
+HUMAN_RESULT_PATH = const.RESULTS_PATH / "human_results"
 
 
 def main():
-    print(f"Using device: {DEVICE}")
+    print(f"Using device: {const.DEVICE}")
 
     human_temp_df, _, _ = read_en_humanitarian_data()
     NUM_LABELS = human_temp_df["class_label"].nunique()
     train_dataset, dev_dataset, test_dataset = get_human_datasets()
 
-    torch.manual_seed(RANDOM_SEED)
+    torch.manual_seed(const.SEED)
     train_dataloader = DataLoader(
-        train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS
+        train_dataset, batch_size=const.BATCH_SIZE, shuffle=True, num_workers=const.NUM_WORKERS
     )
     dev_dataloader = DataLoader(
-        dev_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS
+        dev_dataset, batch_size=const.BATCH_SIZE, shuffle=False, num_workers=const.NUM_WORKERS
     )
 
-    torch.manual_seed(RANDOM_SEED)
-    model = TweetClassifier(num_labels=NUM_LABELS, dropout=DROPOUT).to(DEVICE)
+    torch.manual_seed(const.SEED)
+    model = TweetClassifier(num_labels=NUM_LABELS, dropout=const.DROPOUT).to(const.DEVICE)
     print(model)
 
-    torch.manual_seed(RANDOM_SEED)
-    torch.cuda.manual_seed(RANDOM_SEED)
+    torch.manual_seed(const.SEED)
+    torch.cuda.manual_seed(const.SEED)
 
-    inverse_weights = train_dataset.get_inverse_weights().to(DEVICE)
+    inverse_weights = train_dataset.get_inverse_weights().to(const.DEVICE)
 
     loss_fn = nn.CrossEntropyLoss(weight=inverse_weights)
     optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
@@ -53,19 +46,19 @@ def main():
         test_dataloader=dev_dataloader,
         optimizer=optimizer,
         loss_fn=loss_fn,
-        device=DEVICE,
-        epochs=NUM_EPOCHS,
+        device=const.DEVICE,
+        epochs=const.NUM_EPOCHS,
     )
     print("Training finished.")
 
-    RESULTS_PATH = HUMAN_RESULT_PATH / f"exp_{EXPERIMENT_NR}"
+    RESULTS_PATH = HUMAN_RESULT_PATH / f"exp_{const.EXPERIMENT_NR}"
     os.makedirs(RESULTS_PATH, exist_ok=True)
     print(model_dev_results)
     model_dev_results_df = pd.DataFrame(model_dev_results)
     model_dev_results_df.to_csv(RESULTS_PATH / "model_dev_results.csv", index=False)
     print("Results saved.")
 
-    MODEL_PATH = HUMAN_RESULT_PATH / f"exp_{EXPERIMENT_NR}" / "models"
+    MODEL_PATH = HUMAN_RESULT_PATH / f"exp_{const.EXPERIMENT_NR}" / "models"
     os.makedirs(MODEL_PATH, exist_ok=True)
     MODEL_NAME = "human_classifier.pth"
     torch.save(obj=model.state_dict(), f=MODEL_PATH / MODEL_NAME)
