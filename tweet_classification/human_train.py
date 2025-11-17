2eb1b517bde5bbadd0c1babbbd62a4b968ccad4a
Kazik <kazik@example.com>
2025-11-17 10:19:04 +0100

    removed usage of inverse weights

--- tweet_classification/human_train.py
+++ tweet_classification/human_train.py
@@ -9,8 +9,8 @@ set_seed(const.SEED)
 
 from classifier import TweetClassifier
 from torch import nn
-from torch.utils.data import DataLoader
-from train_test_funcs import train
+from torch.utils.data import DataLoader, WeightedRandomSampler
+from train_test_funcs import FocalLoss, train
 
 HUMAN_RESULT_PATH = const.RESULTS_PATH / "human_results"
 
@@ -22,8 +22,13 @@ def main():
     NUM_LABELS = human_temp_df["class_label"].nunique()
     train_dataset, dev_dataset, test_dataset = get_human_datasets()
 
+    sample_weights = train_dataset.get_sample_weights()
+    sampler = WeightedRandomSampler(
+        sample_weights, num_samples=len(sample_weights), replacement=True
+    )
+
     train_dataloader = DataLoader(
-        train_dataset, batch_size=const.BATCH_SIZE, shuffle=True, num_workers=const.NUM_WORKERS
+        train_dataset, batch_size=const.BATCH_SIZE, sampler=sampler, num_workers=const.NUM_WORKERS
     )
     dev_dataloader = DataLoader(
         dev_dataset, batch_size=const.BATCH_SIZE, shuffle=False, num_workers=const.NUM_WORKERS
@@ -32,10 +37,8 @@ def main():
     model = TweetClassifier(num_labels=NUM_LABELS, dropout=const.DROPOUT).to(const.DEVICE)
     print(model)
 
-    inverse_weights = train_dataset.get_inverse_weights().to(const.DEVICE)
-
-    loss_fn = nn.CrossEntropyLoss(weight=inverse_weights)
-    optimizer = torch.optim.Adam(model.parameters(), lr=2e-5)
+    loss_fn = FocalLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5, weight_decay=0.01)
 
     print("Training is about to start...")
     model_dev_results = train(
