2d3c838ee8c779152dbb7e2171b44175898440da
Kazik <kazik@example.com>
2025-11-17 12:05:00 +0100

    added saving of inference performance

--- tweet_classification/human_test.py
+++ tweet_classification/human_test.py
@@ -3,6 +3,8 @@ import pandas as pd
 import torch
 from utils import get_human_datasets, read_en_humanitarian_data, set_seed
 
+from tweet_classification.constants import HUMAN_EXPERIMENT_NR
+
 set_seed(const.SEED)
 
 from classifier import TweetClassifier
@@ -10,7 +12,7 @@ from torch import nn
 from torch.utils.data import DataLoader, WeightedRandomSampler
 from train_test_funcs import FocalLoss, test_step
 
-HUMAN_RESULTS_PATH = const.RESULTS_PATH / "human_results"
+HUMAN_RESULTS_PATH = const.RESULTS_PATH / "human_results" / f"exp_{const.HUMAN_EXPERIMENT_NR}"
 
 
 def main():
@@ -27,10 +29,7 @@ def main():
     model = TweetClassifier(num_labels=NUM_LABELS, dropout=const.DROPOUT).to(const.DEVICE)
     model.load_state_dict(
         torch.load(
-            HUMAN_RESULTS_PATH
-            / f"exp_{const.HUMAN_EXPERIMENT_NR}"
-            / "models"
-            / f"human_classifier_exp_{const.HUMAN_EXPERIMENT_NR}.pth",
+            HUMAN_RESULTS_PATH / "models" / f"human_classifier_exp_{const.HUMAN_EXPERIMENT_NR}.pth",
             map_location=const.DEVICE,
         )
     )
@@ -41,7 +40,9 @@ def main():
         model=model, test_dataloader=test_dataloader, loss_fn=loss_fn, device=const.DEVICE
     )
 
-    print(test_loss, test_acc)
+    infer_eval_df = pd.DataFrame({"test_loss": test_loss, "test_acc": test_acc})
+
+    infer_eval_df.to_csv(HUMAN_RESULTS_PATH / "infer_eval.csv", index=False)
 
 
 if __name__ == "__main__":
